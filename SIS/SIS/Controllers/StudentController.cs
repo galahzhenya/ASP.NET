c4f0143deaa6b25df871760bdbd1c3bf827c9ee2
galakhevgeniy <nayk333@gmail.com>
2019-05-09 21:35:32 +0300

    Новый проект SIS

---
+++ SIS/SIS/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIS.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddStudent()
+        {
+            return View();
+        }
+
+        public ActionResult EditStudent()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteStudent()
+        {
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ SIS/SIS/StudentRepository.cs
@@ -0,0 +1,30 @@
+﻿using SIS.Models;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SIS
+{
+    public class StudentRepository
+    {
+        private readonly List<Student> _students;
+
+        public StudentRepository()
+        {
+            _students = new List<Student>();
+        }
+
+        public async Task<List<Student>> GetStudents()
+        {
+            return await Task.Run(() => _students);
+        }
+
+        public async Student GetStudents(int id)
+        {
+            return _students.FirstOrDefault(f= >f.Id == Id);
+        }
+    }
+}
