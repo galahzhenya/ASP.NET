d218ce9d9b4d39bf69a4eb9f224f8b0d05f9a791
galakhevgeniy <nayk333@gmail.com>
2019-07-20 02:01:42 +0300

    9 урок

--- Master Progger/MVC_Store/MVC_Store/Areas/Admin/Controllers/ShopController.cs
+++ Master Progger/MVC_Store/MVC_Store/Areas/Admin/Controllers/ShopController.cs
@@ -24,5 +24,72 @@ namespace MVC_Store.Areas.Admin.Controllers
             //Возвращаем List в представление 
             return View(categoryVMList);
         }
+
+        // POST: admin/shop/AddNewCategory
+        public string AddNewCategory(string catName)
+        {
+            //Объявляем переменную строковую ID
+            string id;
+
+            using (Db db = new Db()) {
+                //Проверяем имя категории на уникальность
+                if (db.Categories.Any(x => x.Name == catName)) {
+                    return "titletaken";
+                }
+                //инициализируем модель DTO
+                CategoryDTO dto = new CategoryDTO();
+                //Заполняем данныме в модель
+                dto.Name = catName;
+                dto.Slug = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 100;
+                //Сохраняем данные
+                db.Categories.Add(dto);
+                db.SaveChanges();
+                //Получаем ID, что бы вернуть его в представление 
+                id = dto.Id.ToString();
+            }
+            //Возвращаем ID в представление
+            return id;
+        }
+        //Создаем метот сортировки
+        //POST : "/Admin/Shop/ReorderCategories
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+            using (Db db = new Db()) {
+                //Реализуем начальный счетчик 
+                int count = 1;
+                //Инициализируем модель данных
+                CategoryDTO dto;
+                //Устанавливаем сортировку для каждой страницы
+                foreach (var catId in id) {
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+                    count++;
+                }
+            }
+        }
+
+        //Метод удаления страницы 
+        //GET : /admin/shop/DeleteCategory/
+        public ActionResult DeleteCategory(int id)
+        {
+            using (Db db = new Db()) {
+                //Получение модель категории
+                CategoryDTO dto = db.Categories.Find(id);
+                //Удаление категорию
+                db.Categories.Remove(dto);
+                //Созранение изменений в базе 
+                db.SaveChanges();
+            }
+            //Сообщение об успешном удалении
+            TempData["SM"] = "You have delete a categoty!";
+
+            //Переадресовываем пользователя на страницу Index
+            return RedirectToAction("Categories");
+        }
+
     }
 }
